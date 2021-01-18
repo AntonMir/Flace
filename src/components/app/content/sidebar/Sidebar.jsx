ff8110fe4098e68b7941be233f0f8146606ec94e
AntonMir <toshik124@rambler.ru>
2021-01-18 18:36:57 +0300

    Пытались добавить callback

--- src/components/app/content/sidebar/Sidebar.jsx
+++ src/components/app/content/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Icons from '../../../../assets/img/icons.svg'
 
 import Login from './login/Login.jsx'
@@ -8,21 +8,84 @@ import AddNewPostBtn from './addNewPostBtn/AddNewPostBtn.jsx'
 import BestComment from './bestComment/BestComment.jsx'
 import Advertising from './advertising/Advertising.jsx'
 
+import BurgerMenu from '../../header/burgerMenu/burgerMenu.jsx'
+
 import './sidebar.scss'
 
-export default function Sidebar(props) {
-    return (
-        <>
-            {/*  БОКОВАЯ ПАНЕЛЬ С КНОПКОЙ И РЕКЛАМОЙ  */}
-            <aside className="sidebar">
-                <Login />
-                <User />
-                <NavMenu />
-                <AddNewPostBtn />
-                <BestComment />
-                <Advertising />
-            </aside>
-        </>
-    )
+export default class Sidebar extends Component {
+    constructor(props) {
+        super(props)
+        BurgerMenu.addCallback(
+            this.showSidebar.bind(this))
+    }
+  
+
+    showSidebar() {
+        document.querySelector('.sidebar').classList.toggle('visible');
+    }
+
+
+    render() {
+        return (
+            <>
+            <button style={{order: -1}} onClick={this.showSidebar}>КНОПКААА</button>
+                {/*  БОКОВАЯ ПАНЕЛЬ С КНОПКОЙ И РЕКЛАМОЙ  */}
+                <aside className="sidebar">
+                    <Login />
+                    <User />
+                    <NavMenu />
+                    <AddNewPostBtn />
+                    <BestComment />
+                    <Advertising />
+                </aside>
+            </>
+        )
+    } 
+
 }
+
+//ru.reactjs.org/docs/react-component.html
+
+// export default class Search extends Component {
+
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			values: [],
+// 			age: 123
+// 		}
+// 	}
+
+// 	getAnyInfo = () => {
+// 		anyApi.getInfo()
+// 			.then(
+// 				response => {
+// 					this.setState((state) => {return {
+// 						values: response.data.values
+// 					}})
+// 				}
+// 			)
+// 			.catch(error => {
+// 				console.log(error)
+// 			})
+// 	}
+
+// 	componentWillMount = () => {
+// 		this.getAnyInfo()
+// 	}
+
+// 	render () {
+// 		return (
+// 			<>
+// 				<Dadasd
+// 					lala={this.state.values}
+// 				/>
+
+// 				<Dadasd
+// 					lala={this.state.age}
+// 				/>
+// 			</>
+// 		)
+// 	}
+// }
        
--- src/components/app/header/burgerMenu/burgerMenu.jsx
+++ src/components/app/header/burgerMenu/burgerMenu.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// import Sidebar from '../../content/sidebar/Sidebar.jsx'
+
 import './burgerMenu.scss'
 
-export default function BurgerMenu(props) {
-	return (
-        <div className="menu-toggle" id="menu-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-	)
+export default class BurgerMenu extends Component {
+    constructor(props) {
+        super(props)
+        this.callback = null
+    }
+
+    addCallback(showSidebar) {
+        this.callback = showSidebar
+    }
+
+    show() {
+        try {
+        this.callback()
+        } catch {
+
+        }
+    }
+
+    render() {
+        return (
+            <div onClick={this.show()} className="menu-toggle" id="menu-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        )
+    }
 }
